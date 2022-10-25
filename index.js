4fb6b2c0ba7e517524b461b661ee60e3a3ae5b0b
mintusarker <sarkermintu707@gmail.com>
2022-10-25 23:57:22 +0600

    json file data modified

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const categories = require('./data/Category.json')
+
 app.get('/', (req, res) =>{
     res.send('News API Running')
 });
 
+app.get('/news-categories', (req, res) =>{
+    res.send(categories)
+})
 
 app.listen(port, () => {
-    console.log('Dragon News Server running on port', port)
+    console.log('learning platform Server running on port', port)
 })
