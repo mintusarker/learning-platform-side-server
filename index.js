34b640aaf366aa534f64d2b5fde2363a36720ed0
mintusarker <sarkermintu707@gmail.com>
2022-10-26 00:07:54 +0600

    server side data modified

--- index.js
+++ index.js
@@ -16,12 +16,15 @@ app.get('/news-categories', (req, res) => {
     res.send(categories)
 })
 
-
 app.get('/category/:id', (req, res) => {
     const id = req.params.id;
     const category_news = news.filter(n => n.category_id === id)
     res.send(category_news);
-});
+})
+
+app.get('/news', (req, res) =>{
+    res.send(news);
+})
 
 app.listen(port, () => {
     console.log('learning platform Server running on port', port)
