e6124b556a20e168ff7b579776db75a8163a2ce2
mintusarker <sarkermintu707@gmail.com>
2022-10-26 00:04:19 +0600

    data server site create

--- index.js
+++ index.js
@@ -6,14 +6,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/Category.json')
+const news = require('./data/News.json')
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('News API Running')
 });
 
-app.get('/news-categories', (req, res) =>{
+app.get('/news-categories', (req, res) => {
     res.send(categories)
 })
+
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const category_news = news.filter(n => n.category_id === id)
+    res.send(category_news);
+});
 
 app.listen(port, () => {
     console.log('learning platform Server running on port', port)
